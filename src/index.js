5687125440680954dc7ce17e52e0782bdc6690ad
Yehor Mushet <egor.mushet@gmail.com>
2021-02-13 21:06:02 +0300

    feat: max, min, avg implemented

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array === undefined || array.length === 0) {
+    return 0;
+  }
+  return Math.min(...array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array === undefined || array.length === 0) {
+    return 0;
+  }
+  return Math.max(...array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array === undefined || array.length === 0) {
+    return 0;
+  }
+  return array.reduce((a, b) => a + b, 0) / array.length;
 }
